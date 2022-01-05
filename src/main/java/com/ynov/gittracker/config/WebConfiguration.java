ec3fe98e45657341e8a653228fc11abd9e0edbea
MLivain <matisse.livain@gmail.com>
2022-01-05 13:00:34 +0100

    qsdqsdqsdqsdqs
    qsdqsdqsdqsdqsd

--- src/main/java/com/ynov/gittracker/config/WebConfiguration.java
+++ src/main/java/com/ynov/gittracker/config/WebConfiguration.java
@@ -9,6 +9,7 @@ public class WebConfiguration implements WebMvcConfigurer {
 
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedHeaders("*");
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
     }
 }

be501f3d542f97787ef405489fc05288dde38aee
MLivain <matisse.livain@gmail.com>
2022-01-05 13:10:13 +0100

    qsdqsdqsd
    qsdqsdqsd

--- src/main/java/com/ynov/gittracker/config/WebSecurityConfig.java
+++ src/main/java/com/ynov/gittracker/config/WebSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.ynov.gittracker.config;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -55,26 +58,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return super.authenticationManagerBean();
 	}
 	
+
+	
 	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		// We don't need CSRF for this example
-		httpSecurity
-				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/authenticate", "/register").permitAll().
-				// all other requests need to be authenticated
-						anyRequest().authenticated().and().
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().authorizeRequests()
+        .antMatchers("/authenticate", "/register").permitAll()
+                .anyRequest().authenticated().and()
 				// make sure we use stateless session; session won't be used to
 				// store user's state.
-						exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+						.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+                .and().httpBasic()
+                .and().csrf().disable();
+       // http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+    }
 
-		// Add a filter to validate the tokens with every request
-		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-		// see also: https://docs.spring.io/spring-security/site/docs/5.5.3/reference/html5/#csrf-when
-		httpSecurity.csrf().disable();
-
-        // if Spring MVC is on classpath and no CorsConfigurationSource is provided,
-        // Spring Security will use CORS configuration provided to Spring MVC
-		httpSecurity.cors(Customizer.withDefaults());
-	}
 }
